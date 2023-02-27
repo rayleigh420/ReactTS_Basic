b60bda928d0e42b10f509a9f471723db22410b87
rayleigh420 <nhatduy0409@gmail.com>
2023-02-27 20:05:06 +0700

    Complete props parents to childrens

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,14 +1,14 @@
 import { ChangeEvent, useState } from "react"
 import { IInfo } from "../pages/Page_one"
 
-const Message = ({ name }: { name: string }) => {
+const Message = ({ info }: { info: IInfo }) => {
     const [message, setMessage] = useState<string>('Full-statck Developer')
 
     const changeOption = (e: ChangeEvent<{ value: unknown }>) => setMessage(e.target.value as string)
 
     return (
         <>
-            <p>Name: {name}</p>
+            <p>Name: {info.name} - Progress: {info.progress} - School: {info.school}</p>
             <p>{message}</p>
             <select onChange={changeOption}>
                 <option>Full-stack Developer</option>
--- src/pages/Page_one.tsx
+++ src/pages/Page_one.tsx
@@ -9,17 +9,17 @@ export type IInfo = {
 }
 
 const Page_one = () => {
-    // const info: IInfo = {
-    //     name: 'Le Nhat Duy',
-    //     progress: 'In Progress',
-    //     school: 'University of Science'
-    // }
-    const name: string = "Le Nhat Duy"
+    const info = {
+        name: 'Le Nhat Duy',
+        progress: 'In Progress',
+        school: 'University of Science'
+    }
+    // const name: string = "Le Nhat Duy"
 
     return (
         <>
             <Clock />
-            <Message name={name} />
+            <Message info={info} />
         </>
     )
 }
