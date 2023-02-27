f8185e52a054a28823ff73b300782a33620ed554
rayleigh420 <nhatduy0409@gmail.com>
2023-02-27 09:55:08 +0700

    Try to use props

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,12 +1,14 @@
 import { ChangeEvent, useState } from "react"
+import { IInfo } from "../pages/Page_one"
 
-const Message = () => {
+const Message = ({ name }: { name: string }) => {
     const [message, setMessage] = useState<string>('Full-statck Developer')
 
     const changeOption = (e: ChangeEvent<{ value: unknown }>) => setMessage(e.target.value as string)
 
     return (
         <>
+            <p>Name: {name}</p>
             <p>{message}</p>
             <select onChange={changeOption}>
                 <option>Full-stack Developer</option>
--- src/pages/Page_one.tsx
+++ src/pages/Page_one.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react"
 import Clock from "../components/Clock"
 import Message from "../components/Message"
 
+export type IInfo = {
+    name: string,
+    progress: string,
+    school: string,
+}
+
 const Page_one = () => {
+    // const info: IInfo = {
+    //     name: 'Le Nhat Duy',
+    //     progress: 'In Progress',
+    //     school: 'University of Science'
+    // }
+    const name: string = "Le Nhat Duy"
+
     return (
         <>
             <Clock />
-            <Message />
+            <Message name={name} />
         </>
     )
 }
